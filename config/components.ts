ce81510ae25646dfbc78b51afbd72e7d8a55b7c6
Rohit <dhimanrohit070@gmail.com>
2025-02-20 10:59:58 +0530

    Puck

--- config/components.ts
+++ config/components.ts
@@ -1,13 +1,12 @@
-// config/components.ts
+
 import FormWidget from '../components/FormWidget';
 
 const components = [
-  // Other components can be listed here
   {
     id: 'form',
     name: 'Form',
     component: FormWidget,
-    icon: 'form-icon', // Optional icon reference
+    icon: 'form-icon',
   },
 ];
 
