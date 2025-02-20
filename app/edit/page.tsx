f2f0d3954a93021b248cec71c5c0c30b776b65d6
Rohit <dhimanrohit070@gmail.com>
2025-02-20 10:58:31 +0530

    Puck

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,11 +1,10 @@
 import Editor from '@measured/puck';
 import components from '../../config/components';
 
-// Cast Editor to a React component type
 const EditorComponent = Editor as unknown as React.FC<{ components: any }>;
 
 export default function EditPage() {
-    console.log('Components registry:', components); // Debug line
+    console.log('Components registry:', components); 
   return (
     <div style={{ padding: '1rem' }}>
       <h1>Puck Editor</h1>
