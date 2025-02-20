ce21f059e7557c432c1763bf8f17b8035da54144
Rohit <dhimanrohit070@gmail.com>
2025-02-20 10:59:32 +0530

    Puck

--- components/FormWidget.tsx
+++ components/FormWidget.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 
 const FormWidget = () => {
-  // State to store the list of fields
   const [fields, setFields] = useState([]);
-
-  // Function to add a new field with default properties
   const addField = () => {
     setFields([...fields, { label: '', type: 'textbox' }]);
   };
 
-  // Function to update a field's property (label or type)
   const updateField = (index, key, value) => {
     const updatedFields = fields.map((field, idx) =>
       idx === index ? { ...field, [key]: value } : field
@@ -19,7 +15,6 @@ const FormWidget = () => {
 
   return (
     <div style={{ border: '1px solid #ccc', padding: '1rem' }}>
-      {/* Preview area: displays the form as it would appear */}
       <div className="form-preview">
         {fields.map((field, index) => (
           <div key={index} className="form-field">
@@ -33,7 +28,6 @@ const FormWidget = () => {
         ))}
       </div>
 
-      {/* Properties panel: controls for adding and configuring fields */}
       <div className="form-properties">
         <button onClick={addField}>Add Field</button>
         {fields.map((field, index) => (
@@ -51,7 +45,6 @@ const FormWidget = () => {
               <option value="textbox">Textbox</option>
               <option value="radio">Radio Button</option>
               <option value="checkbox">Checkbox</option>
-              {/* Add more options if needed */}
             </select>
           </div>
         ))}
